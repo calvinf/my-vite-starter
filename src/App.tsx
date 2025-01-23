13b2f1be4200570bfc700cf19a90d463bfc8ba57
Calvin Freitas <cal@calvinfreitas.com>
2025-01-23 11:59:09 -0800

    feat: React 19, Tailwind 4, and other dependency updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <div>Cal was here</div>;
+  return (
+    <main className="max-w-lg m-auto">
+      <div className="p-16 text-center bg-amber-300 border-black border-2 mt-8">
+        <h1 className="text-2xl font-bold">My Vite Starter</h1>
+      </div>
+    </main>
+  );
 }
 
 export default App;
